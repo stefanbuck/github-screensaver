9a631b35db17bd47fdf3c48cf527ff43dce2dace
Stefan Buck <dev@stefanbuck.com>
2014-09-30 07:42:29 +0200

    add first working draft of impress.js

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,16 +1,7 @@
 'use strict';
 
-var qs = require('querystring');
-var $ = require('jquery');
+require('impress');
 
-var queryObject = qs.parse(location.search.substring(1));
-var username = queryObject.username;
-
-if (username) {
-  $('pre').append('loading...');
-  $.get('https://api.github.com/users/' + username).then(function(data) {
-    $('pre').html(JSON.stringify(data, null, ' '));
-  }).fail(function(xhr, status, message) {
-    $('pre').html(message);
-  });
-}
+var instance = impress('stage');
+instance.init();
+setInterval(instance.next, 2000);
